473594586eb84dd8349ce2ba23ac53c3ef14f507
Luke Albert <lukeaalbert@gmail.com>
2025-02-19 21:22:33 -0800

    cleaning up main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 #include<string>
+#include <iostream>
+#include <stdexcept>
 #include "server.h"
 
+void usage() {
+   std::cerr << "Usage: ./proxy-server [SERVER PORT NUMBER]\n";
+}
+
 int main(int argc, char* argv[]) {
-   int port = std::stoi(argv[1]);
-   ProxyServer ps(port);
-   ps.startServer();
-   ps.runServer();
+   if (argc != 2) {
+      usage();
+   }
+   else {
+      try {
+         int port = std::stoi(argv[1]);
+         ProxyServer ps(port);
+         ps.startServer();
+         ps.runServer();
+      }
+      catch (const std::invalid_argument& e) {
+         usage();
+      }
+   }
    return 0;
 }
